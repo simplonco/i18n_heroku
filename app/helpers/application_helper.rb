d430f8eef7258a35361473247933c1f747996722
Mori <keitamori@gmail.com>
2014-02-17 11:52:03 +0100

    Ajout de la methode qui permet de gerer les locales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    def language_selector
+
+
+        if I18n.locale == :en
+            link_to "Fr", url_for(:locale => 'fr')
+        else
+            link_to "Br", url_for(:locale => 'br')
+            link_to "En", url_for(:locale => 'en')
+        end
+    end
 end
